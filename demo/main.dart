192d2ed465130230758a6fd6afc9c3077b62de01
huoxiaoye <17711034@bjtu.edu.cn>
2019-08-20 11:14:34 +0800

    模仿音乐App首页和播放页旋转动画

--- demo/main.dart
+++ demo/main.dart
@@ -8,12 +8,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "音乐播放器动画",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("播放器播放动效"),
-        ),
-        body: PageHome(),
-      ),
+      home: PageHome(),
     );
   }
 }
@@ -42,54 +37,56 @@ class _PageHomeState extends State<PageHome>
 
   @override
   Widget build(BuildContext context) {
-    return 
-
-    
-    Container(
-      child: (Stack(
-        children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height,
-          ),
-          Positioned(
-            bottom: 10,
-            left: 10,
-            child: Hero(
-              tag: "music",
-              child: AnimatedBuilder(
-                animation: _myAnimation,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => new SecondPage()));
-                  },
-                  child: Container(
-                    alignment: Alignment.centerLeft,
-                    height: 80,
-                    width: 80,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(40))),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(40)),
-                      child: Image.network(
-                        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566274424503&di=caabde38683e2b61300f747b20c98c6b&imgtype=0&src=http%3A%2F%2Fpic22.nipic.com%2F20120709%2F10468248_183321228117_2.jpg",
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("播放器播放动效"),
+        ),
+        body: Center(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height,
+              ),
+              Positioned(
+                bottom: 10,
+                left: 10,
+                child: Hero(
+                  tag: "music",
+                  child: AnimatedBuilder(
+                    animation: _myAnimation,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => new SecondPage()));
+                      },
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        height: 80,
                         width: 80,
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(40))),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          child: Image.network(
+                            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566274424503&di=caabde38683e2b61300f747b20c98c6b&imgtype=0&src=http%3A%2F%2Fpic22.nipic.com%2F20120709%2F10468248_183321228117_2.jpg",
+                            width: 80,
+                          ),
+                        ),
                       ),
                     ),
+                    builder: (BuildContext context, Widget child) {
+                      return Transform.rotate(
+                        angle: _myAnimation.value * 2.0 * 3.141592638,
+                        child: child,
+                      );
+                    },
                   ),
                 ),
-                builder: (BuildContext context, Widget child) {
-                  return Transform.rotate(
-                    angle: _myAnimation.value * 2.0 * 3.141592638,
-                    child: child,
-                  );
-                },
-              ),
-            ),
-          )
-        ],
-      )),
-    );
+              )
+            ],
+          ),
+        ));
   }
 }
 
@@ -117,14 +114,13 @@ class _SecondPageState extends State<SecondPage>
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return  Scaffold(
             appBar: AppBar(
               title: Text("播放页面"),
             ),
             floatingActionButton: FloatingActionButton(
               child: Icon(Icons.navigate_before),
-              onPressed: (){
+              onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
@@ -154,6 +150,6 @@ class _SecondPageState extends State<SecondPage>
                           child: child,
                         );
                       },
-                    )))));
+                    ))));
   }
 }
